47ac38132c26c15e42f353ba074de5de624f92d1
Fabian <fabianhiphop.r.a.p.rf@gmail.com>
2021-10-22 14:42:09 -0700

    v1.1

---
+++ hash.sh
@@ -0,0 +1,40 @@
+echo "Menu hash"
+menu () {
+	select opt in sha256sum sha1sum sha512sum md5sum salir;
+	do 
+		case $opt in
+			sha256sum)
+				echo $archivo | sha256sum
+				sleep 5
+				clear
+				;;
+			sha1sum)
+				echo $archivo | sha1sum
+				sleep 5
+				clear
+				;;
+			sha512sum)
+				echo $archivo | sha512sum
+				sleep 5
+				clear
+				;;
+			md5sum)
+				echo $archivo | md5sum
+				sleep 5
+				clear
+				;;
+			salir)
+				break
+				;;
+			*)
+				echo "Introduce una opcion valida"
+				;;
+		esac
+	done
+}
+ls
+echo "Ingresa la frase o el nombre del archivo"
+read archivo
+clear
+echo "Selecciona la tecnica a utilizar"
+menu
--- menu2.sh
+++ menu2.sh
@@ -1,6 +1,6 @@
 echo "Menu de cifrado"
 echo "Fabian"
-select opt in crear cifrar_sim cifrar_asim salir;
+select opt in 'crear usuario' cifrar_sim cifrar_asim salir;
 do
 	case $opt in
 		crear)
@@ -43,12 +43,13 @@ do
 			echo "Cifrado asimetrico"
 			select opt3 in generar_llave verificar_clave exportar_clave importar_clave firmar_archivo cifrar_archivo descifrar_archivo borrar_claves salir;
 			do
-				case $op3 in
+				case $opt3 in
 					generar_llave)
 						gpg --full-generate-key
 						echo "Llave creada exitosamente"
 						;;
 					verificar_clave)
+						gpg -k
 						;;
 					exportar_clave)
 						echo "Claves disponibles"
@@ -62,6 +63,46 @@ do
 						echo "Clave $clave exportada en $archivo_clave"
 						;;
 					importar_clave)
+						echo "Introduce el nombre del archivo con la clave"
+						read archivo_claveimp
+						gpg --import $archivo_claveimp
+						;;
+					firmar_archivo)
+						gpg -k
+						echo "Ingresa el id de la clave que usaras para firmar"
+						read idclave
+						echo "Ingresa el nombre para tu nuevo archivo fimado"
+						read archivofirm
+						echo "Ingresa el nombre del archivo que quieres firmar"
+						read archivo_firm
+						gpg -u $idclave --output $archivofirm --sign $archivo_firm
+						;;
+					cifrar_archivo)
+						gpg -k
+						echo "Ingresa el id de la clave que usaras para firmar"
+						read idclave2
+						echo "Ingresa el nombre del archivo que quieres cifrar"
+						read archivo_firm2
+						gpg -a -r $idclave2 --encrypt $archivo_firm2
+						;;
+					descifrar_archivo)
+						echo "Ingresa el nombre del archivo que quieres descifrar"
+						read archivo_desc2
+						echo "Ingresa un nombre para tu nuevo archivo"
+						read archivo_new
+						gpg -a --decrypt $archivo_desc2 > $archivo_new
+						;;
+					borrar_claves)
+						gpg -k 
+						echo "Ingresa el id de la clave que quieres borrar"
+						read  idborrar
+						gpg --delete-secret-key $idborrar
+						;;
+					salir)
+						break
+						;;
+					*)
+						echo "Introduce una opcion valida"
 						;;
 				esac
 			done
